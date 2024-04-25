a35ceb3572305e5cd105213315f8b0ebb8663d08
mangkoyla <160372172+mangkoyla@users.noreply.github.com>
2024-04-25 12:01:09 +0700

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Nama file backup
-backup_file="backup_$(date +%Y-%m-%d_%H-%M-%S).sql"
+# Nama file backup yang ditentukan
+backup_file="backup.sql"
 
 # Eksekusi pg_dump
 pg_dump --host=floppy.db.elephantsql.com --port=5432 --username=sbabyxcc --password --dbname=sbabyxcc > "$backup_file"
 
 # Mengonfirmasi pesan
 echo "Backup database telah dibuat: $backup_file"
+
+# Memindahkan file backup ke direktori result
+mkdir result
+mv "$backup_file" result/
